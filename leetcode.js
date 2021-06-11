7f347e3ae1c8408337e7fcb2a5c1970e6b4f8204
davidhanenko <d.hanenko24@gmail.com>
2021-06-10 21:27:18 -0400

    Convert digits to letters

--- leetcode.js
+++ leetcode.js
@@ -33,3 +33,55 @@ const minPathSum = function (grid) {
 // );
 
 // ==================================
+
+
+// ==================================
+// Two Sum
+
+// function twoSum(nums, target) {
+//   let res = 0;
+//   for (let i = 0; i < nums.length; i++) {
+//     for (let j = i+1; j < nums.length; j++) {
+//       res = nums[i] + nums[j];
+//       if (res == target) {
+//         return
+//       }
+//     }
+//   }
+
+// };
+
+// console.log(twoSum([2,7,11,15],
+// 9))
+
+// =======================
+// if digit & sum of two inrow digits les than last letter of alphabet(26 => "z") Convert it to letter on this index
+// function convert(num) {
+//   const alphabet = [...Array(26)].map((_, y) => String.fromCharCode(y + 97));
+
+//   const numArr = num.toString().split('');
+//   const resArr = [numArr];
+
+
+//   for (let i = 1; i < numArr.length; i++) {
+//     if (parseInt(numArr[i - 1] + numArr[i]) <= alphabet.length) {
+//       resArr.push(
+//         [
+//           numArr.slice(0, i - 1),
+//           numArr[i - 1] + numArr[i],
+//           numArr.slice(i + 1),
+//         ].flat()
+//       );
+//     }
+//   }
+
+//   for (let i = 0; i < resArr.length; i++) {
+//     resArr[i] = resArr[i].map(el => (el = alphabet[parseInt(el) - 1])).join('');
+//   }
+
+//   return resArr;
+// }
+
+// console.log(convert(11258));
+
+// =================
