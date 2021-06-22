53a0aa1c552e97555f5428292e17b6a200a2f17f
davidhanenko <d.hanenko24@gmail.com>
2021-06-21 21:12:40 -0400

    TwoSum-2 pointers

--- leetcode.js
+++ leetcode.js
@@ -1,28 +1,28 @@
 // ==================================
 // Minimum Path Sum
 
-const minPathSum = function (grid) {
-  let row = grid.length;
-  let col = grid[0].length;
+// const minPathSum = function (grid) {
+//   let row = grid.length;
+//   let col = grid[0].length;
 
-  for (let i = 0; i < row; i++) {
-    for (let j = 0; j < col; j++) {
-      if (i === 0 && j > 0) {
-        grid[i][j] += grid[i][j - 1];
-      }
-      if (i > 0 && j === 0) {
-        grid[i][j] += grid[i - 1][j];
-      }
-      if (i > 0 && j > 0) {
-        grid[i][j] = Math.min(
-          grid[i][j] + grid[i - 1][j],
-          grid[i][j] + grid[i][j - 1]
-        );
-      }
-    }
-  }
-  return grid[row - 1][col - 1];
-};
+//   for (let i = 0; i < row; i++) {
+//     for (let j = 0; j < col; j++) {
+//       if (i === 0 && j > 0) {
+//         grid[i][j] += grid[i][j - 1];
+//       }
+//       if (i > 0 && j === 0) {
+//         grid[i][j] += grid[i - 1][j];
+//       }
+//       if (i > 0 && j > 0) {
+//         grid[i][j] = Math.min(
+//           grid[i][j] + grid[i - 1][j],
+//           grid[i][j] + grid[i][j - 1]
+//         );
+//       }
+//     }
+//   }
+//   return grid[row - 1][col - 1];
+// };
 
 // console.log(
 //   minPathSum([
@@ -56,6 +56,7 @@ const minPathSum = function (grid) {
 
 // =======================
 // if digit & sum of two inrow digits les than last letter of alphabet(26 => "z") Convert it to letter on this index
+
 // function convert(num) {
 //   const alphabet = [...Array(26)].map((_, y) => String.fromCharCode(y + 97));
 
@@ -102,5 +103,29 @@ const minPathSum = function (grid) {
 // findMaxConsecutiveOnes([1,1,0,1,1,1]) // 3
 // =================
 // =================
+// Given an array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number.
+// Return the indices of the two numbers (1-indexed) as an integer array answer of size 2, where 1 <= answer[0] < answer[1] <= numbers.length.
+
+// const twoSum = function(numbers, target) {
+//    let r = numbers.length-1;
+//    let l = 0;
+  
+//     while (l < r) {
+        
+//         let sum = numbers[l] + numbers[r]
+    
+//         if(sum === target) {
+//             return [l+1, r+1];
+            
+//         } else { 
+//             sum > target ? r-- : l++
+       
+//     }
+//     }
+    
+// };
+
+// twoSum([2,7,11,15],9) // [1,2]
 // =================
+
 
