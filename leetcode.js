de28237c2fd61dfb33e28b5482601c038c3a48dc
davidhanenko <d.hanenko24@gmail.com>
2021-08-14 14:44:39 -0400

    pivotIndex

--- leetcode.js
+++ leetcode.js
@@ -189,3 +189,29 @@
 // };
 
 // =============// =============
+
+// =============// =============
+
+// Given an array of integers nums, calculate the pivot index of this array.
+
+// The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
+
+// If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left. This also applies to the right edge of the array.
+
+
+// var pivotIndex = function (nums) {
+//   let left = 0;
+//   let right = nums.reduce((a, b) => a + b, 0) - nums[0];
+//   for (let i = 0; i < nums.length; i++) {
+//     if (left === right) {
+//       return i;
+//     } else {
+//       left += nums[i];
+//       right -= nums[i + 1];
+//     }
+//   }
+//   return -1;
+// };
+
+// =============// =============
+// =============// =============
