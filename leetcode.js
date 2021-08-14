a29ec34593fd611520687b87793f160531d7aa5a
davidhanenko <d.hanenko24@gmail.com>
2021-08-14 15:07:00 -0400

    dominantIndex

--- leetcode.js
+++ leetcode.js
@@ -215,3 +215,24 @@
 
 // =============// =============
 // =============// =============
+
+// You are given an integer array nums where the largest integer is unique.
+
+// Determine whether the largest element in the array is at least twice as much as every other number in the array. If it is, return the index of the largest element, or return -1 otherwise.
+
+// var dominantIndex = function (nums) {
+//   const max = Math.max(...nums);
+//   const index = nums.indexOf(max);
+//   nums.sort((a, b) => a - b);
+//   for (i = nums.length - 2; i > 0; i--) {
+//     if (max / 2 < nums[i]) {
+//       return -1;
+//     }
+//   }
+//   return index;
+// };
+
+
+
+// =============// =============
+// =============// =============
