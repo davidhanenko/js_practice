86576ec7bcdc3d8faca1b89ce0298cf37def938a
davidhanenko <d.hanenko24@gmail.com>
2021-09-28 22:14:51 -0400

    max binary gap

--- codility.js
+++ codility.js
@@ -1,4 +1,6 @@
+// **************
 // Min deletions A & B
+// **************
 
 // function ab(str) {
 //   let count = 0;
@@ -14,6 +16,9 @@
 //       a === false ? count++ : (b = true);
 //     }
 //   }
+//   if (b === false) return -1;
+//   if (a === false) return -1;
+
 //   return count;
 // }
 
@@ -23,8 +28,35 @@
 // ===============================
 // ===============================
 
+// **************
+// Max binary gap
+// **************
 
+// function maxBinaryGap(n) {
+//   const binaryN = n.toString(2);
+//   let maxGap = 0;
+//   let j = 0;
 
+//   for (let i = 0; i < binaryN.length; i++) {
+//     if (binaryN[i] === '1') {
+//       let curGap = i - j - 1;
+//       maxGap = Math.max(maxGap, curGap);
+//       j = i;
+//     }
+//   }
+//   return maxGap;
+// }
+
+// console.log(maxBinaryGap(1041));
+// console.log(maxBinaryGap(20));
+// console.log(maxBinaryGap(32));
+
+// ===============================
+// ===============================
+
+// **************
+
+// **************
 
 // ===============================
 // ===============================
