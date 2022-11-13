5d04f0ab80d97033569e825a32be5f72351e0930
davidhanenko <d.hanenko24@gmail.com>
2022-11-12 19:05:40 -0500

    integer to roman

--- leetcode.js
+++ leetcode.js
@@ -736,7 +736,28 @@ const isPowerOfThree = function (n) {
 
 // =============// =============
 // =============// =============
-
+var romanToInt = function(s) {
+    let res = 0;
+    
+        const obj = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    }
+    
+    for (let i=0; i<s.length; i++) {
+        if( obj[s[i+1]] && obj[s[i]] < obj[s[i+1]]) {
+           res -= obj[s[i]]
+        } else {
+           res += obj[s[i]]
+        }
+    }
+    return res;
+};
 // =============// =============
 // =============// =============
 
