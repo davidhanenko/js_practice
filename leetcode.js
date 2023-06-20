6021402198884a685121a7885735df1c0276362d
davidhanenko <d.hanenko24@gmail.com>
2023-06-19 22:29:19 -0400

    isPalindrome - linked list

--- leetcode.js
+++ leetcode.js
@@ -865,6 +865,20 @@ var reverseList = function (head) {
 // =============// =============
 // =============// =============
 
+var isPalindrome = function (head) {
+  let arr = [];
+
+  while (head) {
+    arr.push(head.val);
+    head = head.next;
+  }
+
+  for (let i = 0; i < arr.length / 2; i++) {
+    if (arr[i] !== arr[arr.length - 1 - i]) return false;
+  }
+  return true;
+};
+
 // =============// =============
 // =============// =============
 
